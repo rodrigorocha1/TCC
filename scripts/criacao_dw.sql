63897da35c31525d8fbaa2bf2a44ee8d7a6722a0
rrrocha <silva.rodrigo31@outlook.com>
2020-04-25 21:14:02 -0300

    Criação das dimensões data_abertura, data_fechamento e problema

--- scripts/criacao_dw.sql
+++ scripts/criacao_dw.sql
@@ -1,121 +1,186 @@
-CREATE TABLE dim_data (
-                sk_data NUMBER NOT NULL,
-                data_completa DATE NOT NULL,
-                desc_data_completa VARCHAR2(60) NOT NULL,
-                nr_ano VARCHAR2(60) NOT NULL,
-                nm_trimestre VARCHAR2(20) NOT NULL,
-                nr_ano_trimestre VARCHAR2(20) NOT NULL,
-                nr_mes NUMBER NOT NULL,
-                nm_mes VARCHAR2(60) NOT NULL,
-                ano_mes VARCHAR2(60) NOT NULL,
-                nr_semana NUMBER NOT NULL,
-                ano_semana VARCHAR2(60) NOT NULL,
-                nr_dia NUMBER NOT NULL,
-                nr_dia_ano VARCHAR2(30) NOT NULL,
-                nm_dia_semana VARCHAR2(60) NOT NULL,
-                flag_final_semana VARCHAR2(10) NOT NULL,
-                dt_carga DATE DEFAULT SYSDATE NOT NULL,
-                CONSTRAINT DIM_DATA_PK PRIMARY KEY (sk_data)
+
+CREATE TABLE dim_problema (
+                sk_problema INTEGER NOT NULL,
+                nk_problema INTEGER NOT NULL,
+                desc_problema VARCHAR(60) NOT NULL,
+                CONSTRAINT dim_problema_pk PRIMARY KEY (sk_problema)
 );
 
 
-COMMENT ON COLUMN dim_data.sk_data IS '20160101';
-COMMENT ON COLUMN dim_data.data_completa IS '01/01/2017';
-COMMENT ON COLUMN dim_data.desc_data_completa IS 'Descrição da data completa, mês escrito por extenso. Ex: 01 janeiro de 2017';
-COMMENT ON COLUMN dim_data.nr_ano IS 'Número do Ano. Ex: 2017';
-COMMENT ON COLUMN dim_data.nm_trimestre IS 'Número do trimestre. Em que trimestre do ano
+CREATE TABLE dim_data_abertura (
+                sk_data_abertura INTEGER NOT NULL,
+                data_abertura DATE NOT NULL,
+                desc_data_completa VARCHAR(60) NOT NULL,
+                nr_ano VARCHAR(2) NOT NULL,
+                nm_trimestre VARCHAR(20) NOT NULL,
+                nr_ano_trimestre VARCHAR(20) NOT NULL,
+                nr_mes INTEGER NOT NULL,
+                nm_mes VARCHAR(60) NOT NULL,
+                ano_mes VARCHAR(60) NOT NULL,
+                nr_semana INTEGER NOT NULL,
+                ano_semana VARCHAR(60) NOT NULL,
+                nr_dia INTEGER NOT NULL,
+                nr_dia_ano VARCHAR(30) NOT NULL,
+                nm_dia_semana VARCHAR(60) NOT NULL,
+                flag_final_semana VARCHAR(10) NOT NULL,
+                dt_carga DATE NOT NULL,
+                CONSTRAINT dim_data_abertura_pk PRIMARY KEY (sk_data_abertura)
+);
+
+
+COMMENT ON COLUMN dim_data_abertura.sk_data_abertura IS '20160101';
+COMMENT ON COLUMN dim_data_abertura.data_abertura IS '01/01/2017';
+COMMENT ON COLUMN dim_data_abertura.desc_data_completa IS 'Descrição da data completa, mês escrito por extenso. Ex: 01 janeiro de 2017';
+COMMENT ON COLUMN dim_data_abertura.nr_ano IS 'Número do Ano. Ex: 2017';
+COMMENT ON COLUMN dim_data_abertura.nm_trimestre IS 'Número do trimestre. Em que trimestre do ano
 aquela data está. EX:1T';
-COMMENT ON COLUMN dim_data.nr_ano_trimestre IS 'Ano e número do trimestre, é como uma
+COMMENT ON COLUMN dim_data_abertura.nr_ano_trimestre IS 'Ano e número do trimestre, é como uma
 junção dos dois campos anteriores. Ex: 2017/1T';
-COMMENT ON COLUMN dim_data.nr_mes IS 'Número do mês. ex: 01';
-COMMENT ON COLUMN dim_data.nm_mes IS 'Nome do mês. Ex: Janeiro';
-COMMENT ON COLUMN dim_data.ano_mes IS 'Ano e número do mês juntos. Ex: 2017/01';
-COMMENT ON COLUMN dim_data.nr_semana IS 'Número daquela semana no ano. Ex: 34';
-COMMENT ON COLUMN dim_data.ano_semana IS 'Ano e número da semana juntos. Ex: 2017/34';
-COMMENT ON COLUMN dim_data.nr_dia IS 'Número do dia no mês Ex: 31';
-COMMENT ON COLUMN dim_data.nr_dia_ano IS 'Ano e número da semana juntos. Ex: 2017/31';
-COMMENT ON COLUMN dim_data.nm_dia_semana IS 'Nome do dia da semana. Ex: quinta-feira';
-COMMENT ON COLUMN dim_data.flag_final_semana IS 'Flag para indicar se é final de semana ou
+COMMENT ON COLUMN dim_data_abertura.nr_mes IS 'Número do mês. ex: 01';
+COMMENT ON COLUMN dim_data_abertura.nm_mes IS 'Nome do mês. Ex: Janeiro';
+COMMENT ON COLUMN dim_data_abertura.ano_mes IS 'Ano e número do mês juntos. Ex: 2017/01';
+COMMENT ON COLUMN dim_data_abertura.nr_semana IS 'Número daquela semana no ano. Ex: 34';
+COMMENT ON COLUMN dim_data_abertura.ano_semana IS 'Ano e número da semana juntos. Ex: 2017/34';
+COMMENT ON COLUMN dim_data_abertura.nr_dia IS 'Número do dia no mês Ex: 31';
+COMMENT ON COLUMN dim_data_abertura.nr_dia_ano IS 'Ano e número da semana juntos. Ex: 2017/31';
+COMMENT ON COLUMN dim_data_abertura.nm_dia_semana IS 'Nome do dia da semana. Ex: quinta-feira';
+COMMENT ON COLUMN dim_data_abertura.flag_final_semana IS 'Flag para indicar se é final de semana ou
 não. Ex: Sim';
 
 
-CREATE SEQUENCE DIM_CNAE_SK_CNAE_SEQ;
+CREATE TABLE dim_estado (
+                sk_estado INTEGER NOT NULL,
+                nk_estado VARCHAR(2) NOT NULL,
+                nm_estado VARCHAR(60) NOT NULL,
+                CONSTRAINT dim_estado_pk PRIMARY KEY (sk_estado)
+);
+
+
+CREATE TABLE dim_data_arquivamento (
+                sk_data_arquivamento INTEGER NOT NULL,
+                data DATE NOT NULL,
+                desc_data_completa VARCHAR(60) NOT NULL,
+                nr_ano VARCHAR(2) NOT NULL,
+                nm_trimestre VARCHAR(20) NOT NULL,
+                nr_ano_trimestre VARCHAR(20) NOT NULL,
+                nr_mes INTEGER NOT NULL,
+                nm_mes VARCHAR(60) NOT NULL,
+                ano_mes VARCHAR(60) NOT NULL,
+                nr_semana INTEGER NOT NULL,
+                ano_semana VARCHAR(60) NOT NULL,
+                nr_dia INTEGER NOT NULL,
+                nr_dia_ano VARCHAR(30) NOT NULL,
+                nm_dia_semana VARCHAR(60) NOT NULL,
+                flag_final_semana VARCHAR(10) NOT NULL,
+                dt_carga DATE NOT NULL,
+                CONSTRAINT dim_data_arquivamento_pk PRIMARY KEY (sk_data_arquivamento)
+);
+COMMENT ON COLUMN dim_data_arquivamento.sk_data_arquivamento IS '20160101';
+COMMENT ON COLUMN dim_data_arquivamento.data IS '01/01/2017';
+COMMENT ON COLUMN dim_data_arquivamento.desc_data_completa IS 'Descrição da data completa, mês escrito por extenso. Ex: 01 janeiro de 2017';
+COMMENT ON COLUMN dim_data_arquivamento.nr_ano IS 'Número do Ano. Ex: 2017';
+COMMENT ON COLUMN dim_data_arquivamento.nm_trimestre IS 'Número do trimestre. Em que trimestre do ano
+aquela data está. EX:1T';
+COMMENT ON COLUMN dim_data_arquivamento.nr_ano_trimestre IS 'Ano e número do trimestre, é como uma
+junção dos dois campos anteriores. Ex: 2017/1T';
+COMMENT ON COLUMN dim_data_arquivamento.nr_mes IS 'Número do mês. ex: 01';
+COMMENT ON COLUMN dim_data_arquivamento.nm_mes IS 'Nome do mês. Ex: Janeiro';
+COMMENT ON COLUMN dim_data_arquivamento.ano_mes IS 'Ano e número do mês juntos. Ex: 2017/01';
+COMMENT ON COLUMN dim_data_arquivamento.nr_semana IS 'Número daquela semana no ano. Ex: 34';
+COMMENT ON COLUMN dim_data_arquivamento.ano_semana IS 'Ano e número da semana juntos. Ex: 2017/34';
+COMMENT ON COLUMN dim_data_arquivamento.nr_dia IS 'Número do dia no mês Ex: 31';
+COMMENT ON COLUMN dim_data_arquivamento.nr_dia_ano IS 'Ano e número da semana juntos. Ex: 2017/31';
+COMMENT ON COLUMN dim_data_arquivamento.nm_dia_semana IS 'Nome do dia da semana. Ex: quinta-feira';
+COMMENT ON COLUMN dim_data_arquivamento.flag_final_semana IS 'Flag para indicar se é final de semana ou
+não. Ex: Sim';
+
 
 CREATE TABLE dim_cnae (
-                sk_cnae NUMBER NOT NULL,
-                nk_cnae NUMBER NOT NULL,
-                nm_cnae VARCHAR2(120) NOT NULL,
-                desc_cnae_principal VARCHAR2(120) NOT NULL,
-                CONSTRAINT DIM_CNAE_PK PRIMARY KEY (sk_cnae)
+                sk_cnae INTEGER NOT NULL,
+                nk_cnae INTEGER NOT NULL,
+                nm_cnae VARCHAR(120) NOT NULL,
+                desc_cnae_principal VARCHAR(120) NOT NULL,
+                CONSTRAINT dim_cnae_pk PRIMARY KEY (sk_cnae)
 );
 
-
-CREATE SEQUENCE DIM_EMPRESAS_SK_EMPRESAS_SEQ;
 
 CREATE TABLE dim_empresas (
-                sk_empresas NUMBER NOT NULL,
-                nk_cnpj NUMBER NOT NULL,
-                razao_social VARCHAR2(240) NOT NULL,
-                nome_fantasia VARCHAR2(120) NOT NULL,
-                CONSTRAINT DIM_EMPRESAS_PK PRIMARY KEY (sk_empresas)
+                sk_empresas INTEGER NOT NULL,
+                nk_cnpj INTEGER NOT NULL,
+                razao_social VARCHAR(240) NOT NULL,
+                nome_fantasia VARCHAR(120) NOT NULL,
+                CONSTRAINT dim_empresas_pk PRIMARY KEY (sk_empresas)
 );
 
-
-CREATE SEQUENCE DIM_REGIAO_SK_REGIAO_SEQ;
 
 CREATE TABLE dim_regiao (
-                sk_regiao NUMBER NOT NULL,
-                nk_codigo_regiao NUMBER NOT NULL,
-                nm_regiao VARCHAR2(60) NOT NULL,
-                nm_estado VARCHAR2(2) NOT NULL,
-                CONSTRAINT DIM_REGIAO_PK PRIMARY KEY (sk_regiao)
+                sk_regiao INTEGER NOT NULL,
+                nk_codigo_regiao INTEGER NOT NULL,
+                nm_regiao VARCHAR(60) NOT NULL,
+                CONSTRAINT dim_regiao_pk PRIMARY KEY (sk_regiao)
 );
 
 
-CREATE SEQUENCE DIM_ASSUNTO_SK_ASSUNTO_SEQ;
-
 CREATE TABLE dim_assunto (
-                sk_assunto NUMBER NOT NULL,
-                nk_codigo_assunto NUMBER NOT NULL,
-                desc_assunto VARCHAR2(120) NOT NULL,
-                CONSTRAINT DIM_ASSUNTO_PK PRIMARY KEY (sk_assunto)
+                sk_assunto INTEGER NOT NULL,
+                nk_codigo_assunto INTEGER NOT NULL,
+                desc_assunto VARCHAR(120) NOT NULL,
+                CONSTRAINT dim_assunto_pk PRIMARY KEY (sk_assunto)
 );
 
 
 CREATE TABLE ft_reclamacao (
-                sk_assunto NUMBER NOT NULL,
-                sk_regiao NUMBER NOT NULL,
-                sk_empresas NUMBER NOT NULL,
-                sk_cnae NUMBER NOT NULL,
-                sk_data NUMBER NOT NULL,
-                flag_atendida VARCHAR(1) NOT NULL
+                sk_data_abertura INTEGER NOT NULL,
+                sk_data_arquivamento INTEGER NOT NULL,
+                sk_problema INTEGER NOT NULL,
+                sk_assunto INTEGER NOT NULL,
+                sk_cnae INTEGER NOT NULL,
+                sk_regiao INTEGER NOT NULL,
+                sk_estado INTEGER NOT NULL,
+                sk_empresas INTEGER NOT NULL,
+                flag_atendida CHAR(1) NOT NULL
 );
-
-COMMENT ON COLUMN ft_reclamacao.sk_data IS '20160101';
+COMMENT ON COLUMN ft_reclamacao.sk_data_abertura IS '20160101';
+COMMENT ON COLUMN ft_reclamacao.sk_data_arquivamento IS '20160101';
 COMMENT ON COLUMN ft_reclamacao.flag_atendida IS 'Mostra se a reclamação foi atendida ou não';
 
 
-ALTER TABLE ft_reclamacao ADD CONSTRAINT DIM_DATA_FT_RECLAMACAO_FK
-FOREIGN KEY (sk_data)
-REFERENCES dim_data (sk_data)
+ALTER TABLE ft_reclamacao ADD CONSTRAINT dim_problema_ft_reclamacao_fk
+FOREIGN KEY (sk_problema)
+REFERENCES dim_problema (sk_problema)
 NOT DEFERRABLE;
 
-ALTER TABLE ft_reclamacao ADD CONSTRAINT DIM_CNAE_FT_RECLAMACAO_FK
+ALTER TABLE ft_reclamacao ADD CONSTRAINT dim_dt_abertura_ft_recl_fk
+FOREIGN KEY (sk_data_abertura)
+REFERENCES dim_data_abertura (sk_data_abertura)
+NOT DEFERRABLE;
+
+ALTER TABLE ft_reclamacao ADD CONSTRAINT dim_estado_ft_reclamacao_fk
+FOREIGN KEY (sk_estado)
+REFERENCES dim_estado (sk_estado)
+NOT DEFERRABLE;
+
+ALTER TABLE ft_reclamacao ADD CONSTRAINT dim_data_ft_reclamacao_fk
+FOREIGN KEY (sk_data_arquivamento)
+REFERENCES dim_data_arquivamento (sk_data_arquivamento)
+NOT DEFERRABLE;
+
+ALTER TABLE ft_reclamacao ADD CONSTRAINT dim_cnae_ft_reclamacao_fk
 FOREIGN KEY (sk_cnae)
 REFERENCES dim_cnae (sk_cnae)
 NOT DEFERRABLE;
 
-ALTER TABLE ft_reclamacao ADD CONSTRAINT DIM_EMPRESAS_FT_RECLAMACAO_FK
+ALTER TABLE ft_reclamacao ADD CONSTRAINT dim_empresas_ft_reclamacao_fk
 FOREIGN KEY (sk_empresas)
 REFERENCES dim_empresas (sk_empresas)
 NOT DEFERRABLE;
 
-ALTER TABLE ft_reclamacao ADD CONSTRAINT DIM_REGIAO_FT_RECLAMACAO_FK
+ALTER TABLE ft_reclamacao ADD CONSTRAINT dim_regiao_ft_reclamacao_fk
 FOREIGN KEY (sk_regiao)
 REFERENCES dim_regiao (sk_regiao)
 NOT DEFERRABLE;
 
-ALTER TABLE ft_reclamacao ADD CONSTRAINT DIM_ASSUNTO_FT_RECLAMACAO_FK
+ALTER TABLE ft_reclamacao ADD CONSTRAINT dim_assunto_ft_reclamacao_fk
 FOREIGN KEY (sk_assunto)
 REFERENCES dim_assunto (sk_assunto)
 NOT DEFERRABLE;
